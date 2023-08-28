90ce48c9a7686c1e25cbb9893387fdeb4c122a99
Crazy <contact@csjako.de>
2023-08-28 16:36:27 +0200

    Added feature/controls

--- core/src/main/java/net/crazy/streamchat/core/StreamChat.java
+++ core/src/main/java/net/crazy/streamchat/core/StreamChat.java
@@ -1,23 +1,39 @@
 package net.crazy.streamchat.core;
 
 import lombok.Getter;
+import net.crazy.streamchat.core.activity.ControlNavigationElement;
 import net.crazy.streamchat.core.generated.DefaultReferenceStorage;
 import net.crazy.streamchat.core.listener.TwitchChatListener;
 import net.labymod.api.addon.LabyAddon;
+import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
 import net.labymod.api.models.addon.annotation.AddonMain;
+import java.util.ArrayList;
 
 @AddonMain
 public class StreamChat extends LabyAddon<Configuration> {
 
+  public static StreamChat addon;
+
   @Getter
   private TwitchBot bot;
 
+  @Getter
+  private final ArrayList<ComponentWidget> messageHistory = new ArrayList<>();
+
   @Override
   protected void enable() {
+    addon = this;
     this.registerSettingCategory();
+
+    DefaultReferenceStorage storage = this.referenceStorageAccessor();
+
     bot = new TwitchBot(this);
 
     this.registerListener(new TwitchChatListener(this));
+
+    if (configuration().getControlPanel().get()) {
+      this.labyAPI().navigationService().register(new ControlNavigationElement(this));
+    }
 
     this.logger().info(String.format("StreamChat+ | Addon successfully enabled. (v%s)",
         this.addonInfo().getVersion()));
---
+++ core/src/main/java/net/crazy/streamchat/core/activity/ControlActivity.java
@@ -0,0 +1,123 @@
+package net.crazy.streamchat.core.activity;
+
+import net.crazy.streamchat.core.StreamChat;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.client.gui.mouse.MutableMouse;
+import net.labymod.api.client.gui.screen.Parent;
+import net.labymod.api.client.gui.screen.activity.AutoActivity;
+import net.labymod.api.client.gui.screen.activity.Link;
+import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
+import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.input.ButtonWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.entry.HorizontalListEntry;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.list.HorizontalListWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.list.VerticalListWidget;
+import net.labymod.api.client.render.matrix.Stack;
+import net.labymod.api.client.resources.ResourceLocation;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@AutoActivity
+@Link("control.lss")
+public class ControlActivity extends SimpleActivity {
+
+  private final StreamChat addon;
+  private final ResourceLocation textures = ResourceLocation.create("streamchat",
+      "themes/vanilla/textures/controls.png");
+
+  public ControlActivity(StreamChat addon) {
+    this.addon = addon;
+  }
+
+  private ButtonWidget statusButton, restartButton;
+  private final Icon startIcon = Icon.sprite16(textures, 0, 0),
+      stopIcon = Icon.sprite16(textures, 1, 0),
+      restartIcon = Icon.sprite16(textures, 2, 0);
+
+  VerticalListWidget<ComponentWidget> messageHistory;
+
+  @Override
+  public void initialize(Parent parent) {
+    super.initialize(parent);
+
+    boolean isConnected = addon.getBot().isConnected();
+
+
+    HorizontalListWidget controlList = new HorizontalListWidget();
+    controlList.addId("controls");
+
+    /* Buttons */
+    statusButton = ButtonWidget.text(!isConnected ? "Start" : "Stop", this::changeStatus);
+    statusButton.updateIcon(!isConnected ? startIcon : stopIcon);
+    statusButton.addId("statusButton");
+    controlList.addChild(new HorizontalListEntry(statusButton));
+
+    restartButton = ButtonWidget.text("Restart", this::restart);
+    restartButton.updateIcon(restartIcon);
+    restartButton.addId("restartButton");
+    controlList.addChild(new HorizontalListEntry(restartButton));
+    if (!addon.getBot().isConnected())
+      restartButton.setEnabled(false);
+
+    document().addChild(controlList);
+
+    /* Messages */
+    this.messageHistory = new VerticalListWidget<>();
+    messageHistory.addId("messageHistory");
+
+    for (ComponentWidget widget : addon.getMessageHistory())
+      messageHistory.addChild(widget);
+
+    document().addChild(messageHistory);
+  }
+
+  @Override
+  public void render(Stack stack, MutableMouse mouse, float tickDelta) {
+    super.render(stack, mouse, tickDelta);
+
+    int shownMessages = messageHistory.getChildren().size();
+    int availableMessages = addon.getMessageHistory().size();
+
+    if (shownMessages >= availableMessages)
+      return;
+
+
+    for (int i = Math.max(shownMessages - 1, 0);
+         messageHistory.getChildren().size() != addon.getMessageHistory().size(); i++) {
+      ComponentWidget widget = addon.getMessageHistory().get(i);
+      messageHistory.addChild(widget);
+    }
+
+    this.reload();
+  }
+
+  private void changeStatus() {
+    if (addon.getBot().isConnected()) {
+      addon.getBot().stop();
+      this.statusButton.updateComponent(Component.text("Start"));
+      this.statusButton.updateIcon(startIcon);
+      this.restartButton.setEnabled(false);
+      return;
+    }
+
+    addon.getBot().start();
+    this.statusButton.updateComponent(Component.text("Stop"));
+    this.statusButton.updateIcon(stopIcon);
+    this.restartButton.setEnabled(true);
+  }
+
+  private void restart() {
+    this.statusButton.setEnabled(false);
+    this.restartButton.setEnabled(false);
+
+    addon.getBot().restart();
+
+    final ScheduledExecutorService se = Executors.newSingleThreadScheduledExecutor();
+    se.schedule(() -> {
+      this.statusButton.setEnabled(true);
+      this.restartButton.setEnabled(true);
+    }, 3, TimeUnit.SECONDS);
+  }
+}
