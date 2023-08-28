613eb7695a2bc98cc7c733c9ba5c547960c92f6c
Crazy <contact@csjako.de>
2023-08-28 09:21:11 +0200

    Added Lombok

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,6 +7,11 @@ plugins {
 dependencies {
     api(project(":api"))
     maven(mavenCentral(), "pircbot:pircbot:1.5.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    testCompileOnly("org.projectlombok:lombok:1.18.28")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
 }
 
 labyModProcessor {
